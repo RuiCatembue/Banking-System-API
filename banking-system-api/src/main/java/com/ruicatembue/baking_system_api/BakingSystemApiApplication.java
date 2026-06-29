38a905557be738537d5090e2bae79ec1100e9172
Rui Catembue <126804664+RuiCatembue@users.noreply.github.com>
2026-06-29 17:20:27 +0100

    Initial project structure

---
+++ banking-system-api/src/main/java/com/ruicatembue/baking_system_api/BakingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.ruicatembue.baking_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakingSystemApiApplication.class, args);
+	}
+
+}
---
+++ banking-system-api/src/test/java/com/ruicatembue/baking_system_api/BankingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruicatembue.baking_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
